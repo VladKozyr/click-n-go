e0cc9afee2f2f2a9d5b903ff42f034158134f2ca
Danylo Makarenko <danylo.makarenko@ukma.edu.ua>
2021-03-14 13:38:14 +0200

    - Change auth logo - Update ProfileActivity

--- app/src/main/java/com/clickandgo/ProfileActivity.java
+++ app/src/main/java/com/clickandgo/ProfileActivity.java
@@ -1,39 +1,60 @@
 package com.clickandgo;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
-import com.clickandgo.ui.auth.FragmentAdapter;
+import com.bumptech.glide.Glide;
 import com.clickandgo.ui.profile.ProfileFragmentAdapter;
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.material.tabs.TabLayout;
 
 public class ProfileActivity extends AppCompatActivity {
-    private ProfileFragmentAdapter fragmentAdapter;
-    private ViewPager viewPager;
-
-
+    private ImageView userPhoto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        fragmentAdapter = new ProfileFragmentAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, this);
-        viewPager = findViewById(R.id.profileViewPager);
+        ProfileFragmentAdapter fragmentAdapter = new ProfileFragmentAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, this);
+        ViewPager viewPager = findViewById(R.id.profileViewPager);
         viewPager.setAdapter(fragmentAdapter);
 
         TabLayout tabLayout = findViewById(R.id.profileTabLayout);
-        findViewById(R.id.arrow).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        findViewById(R.id.arrow).setOnClickListener(view -> finish());
+
+        userPhoto = findViewById(R.id.imageViewUserPhoto);
+        userPhoto.setOnClickListener(view -> ImagePicker.Companion.with(ProfileActivity.this)
+                .crop(1,1)
+                .start());
+
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            assert data != null;
+            Uri imageUri = data.getData();
+            Glide.with(this)
+                    .load(imageUri)
+                    .circleCrop()
+                    .into(userPhoto);
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(this, ImagePicker.Companion.getError(data), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onBackArrowClick(View view) {
