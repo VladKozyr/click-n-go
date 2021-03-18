c06ca29e8f1413e304f7b4899d0e2d2aad7d67c6
rlaktionov <rlaktionov@trading-point.com>
2021-03-18 08:37:58 +0200

    - documentation added;

--- app/src/main/java/com/clickandgo/usecase/PlaceResultsUseCase.java
+++ app/src/main/java/com/clickandgo/usecase/PlaceResultsUseCase.java
@@ -2,7 +2,6 @@ package com.clickandgo.usecase;
 
 import android.util.Log;
 
-import androidx.arch.core.util.Function;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
@@ -17,10 +16,16 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Represent entry point to communicate over lists of {@link PlaceResult}, which
+ * are could exist in to states: <b>FAVORITES</b> and <b>HISTORY</b>
+ */
 public class PlaceResultsUseCase {
 
+    /* Singleton implementation */
     private static PlaceResultsUseCase instance;
 
+    /* Singleton implementation */
     public static synchronized PlaceResultsUseCase getInstance() {
         if (instance == null) {
             instance = new PlaceResultsUseCase();
@@ -28,7 +33,9 @@ public class PlaceResultsUseCase {
         return instance;
     }
 
-    private PlaceResultsUseCase() { }
+    /* Singleton implementation */
+    private PlaceResultsUseCase() {
+    }
 
     private final UserRepository repository = UserRepository.getInstance();
     private final LinkedList<PlaceResult> history = new LinkedList<>();
@@ -38,31 +45,33 @@ public class PlaceResultsUseCase {
     private final MutableLiveData<List<PlaceResult>> historyLiveData = new MutableLiveData<>();
 
     public LiveData<List<PlaceResult>> observeFavorites() {
-        return Transformations.switchMap(
-                repository.getFavourites(),
-                (Function<List<DocumentReference>, LiveData<List<PlaceResult>>>) input -> {
-                    favourites.clear();
-                    for (DocumentReference result : input) {
-                        favourites.add(mapToPlaceResult(result, true));
-                    }
-                    favoritesLiveData.setValue(favourites);
-                    return favoritesLiveData;
-                });
+        return Transformations.switchMap(repository.getFavourites(), input -> {
+            favourites.clear();
+            for (DocumentReference result : input) {
+                favourites.add(mapToPlaceResult(result, true));
+            }
+            favoritesLiveData.setValue(favourites);
+            return favoritesLiveData;
+        });
     }
 
     public LiveData<List<PlaceResult>> observeHistory() {
-        return Transformations.switchMap(
-                repository.getHistory(),
-                (Function<List<DocumentReference>, LiveData<List<PlaceResult>>>) input -> {
-                    history.clear();
-                    for (DocumentReference result : input) {
-                        history.add(mapToPlaceResult(result, isDocumentsPresentInFavorites(result)));
-                    }
-                    historyLiveData.setValue(history);
-                    return historyLiveData;
-                });
+        return Transformations.switchMap(repository.getHistory(), input -> {
+            history.clear();
+            for (DocumentReference result : input) {
+                history.add(mapToPlaceResult(result, isDocumentsPresentInFavorites(result)));
+            }
+            historyLiveData.setValue(history);
+            return historyLiveData;
+        });
     }
 
+    /**
+     * Modifies both (favorite and history) lists to change {@link PlaceResult#isLiked()}
+     * flag in <b>true</b> and notifies live data about changes.
+     *
+     * @param data where flag must be updated.
+     */
     public void addFavourite(PlaceResult data) {
         updateHistory(data, true);
         favourites.addFirst(data);
@@ -70,6 +79,12 @@ public class PlaceResultsUseCase {
         historyLiveData.postValue(history);
     }
 
+    /**
+     * Modifies both (favorite and history) lists to change {@link PlaceResult#isLiked()}
+     * flag in <b>false</b> and notifies live data about changes.
+     *
+     * @param data where flag must be updated.
+     */
     public void removeFavourite(PlaceResult data) {
         updateHistory(data, false);
         favourites.remove(data);
@@ -77,21 +92,22 @@ public class PlaceResultsUseCase {
         historyLiveData.postValue(history);
     }
 
+    /**
+     * Notifies repository to update {@link FirebaseStorage}
+     */
     public void updateRemoteDataBase() {
-        ArrayList<DocumentReference> historyRef = new ArrayList<>(history.size());
-        ArrayList<DocumentReference> favoritesRef = new ArrayList<>(favourites.size());
-        for (PlaceResult result : history) {
-            historyRef.add(mapFromPlaceResult(result));
-        }
-        for (PlaceResult result : favourites) {
-            favoritesRef.add(mapFromPlaceResult(result));
-        }
-        repository.updateFavourites(favoritesRef);
-        repository.updateHistory(historyRef);
+        repository.updateFavourites(mapToDocumentReferenceList(favourites));
+        repository.updateHistory(mapToDocumentReferenceList(history));
     }
 
-    private void updateHistory(PlaceResult data, boolean isLiked) {
-        int indexInHistory = history.indexOf(data);
+    /**
+     * Finds current place result in history list and modifies it {@link PlaceResult#isLiked()} flag.
+     *
+     * @param result  to find matches
+     * @param isLiked new value of place's property
+     */
+    private void updateHistory(PlaceResult result, boolean isLiked) {
+        int indexInHistory = history.indexOf(result);
         if (indexInHistory != -1) {
             PlaceResult resultInHistory = history.get(indexInHistory);
             resultInHistory.setLiked(isLiked);
@@ -121,6 +137,14 @@ public class PlaceResultsUseCase {
 
     private DocumentReference mapFromPlaceResult(PlaceResult result) {
         return result.getReference();
+    }
+
+    private List<DocumentReference> mapToDocumentReferenceList(List<PlaceResult> sourceList) {
+        ArrayList<DocumentReference> resultList = new ArrayList<>(sourceList.size());
+        for (PlaceResult source : sourceList) {
+            resultList.add(mapFromPlaceResult(source));
+        }
+        return resultList;
     }
 
     private boolean isDocumentsPresentInFavorites(DocumentReference reference) {
